ad78d2cd6d8ef542f3e2041b19dfeabdb2d5abbf
Kevin <kevin860612@gmail.com>
2019-10-07 20:55:23 +0800

    Handle Support but Sort Headtable

--- fpg.go
+++ fpg.go
@@ -17,16 +17,16 @@ type FoodLink struct {
 	Count int
 	Links []*Node
 }
+
 type Foodlist map[string]FoodLink
 
-func fptreeCreadeNode(food string, parent *Node) *Node {
+type Dataset map[int][]string
+
+func fpTreeCreadeNode(food string, parent *Node) *Node {
 	return &Node{food, 1, parent, []*Node{}}
 }
 
-func (rootp *Node) fptreeInsert(listp *Foodlist, food string) (node *Node) {
-	list := *listp
-	link := list[food]
-	link.Count++
+func (rootp *Node) fpTreeInsert(list Foodlist, food string) (node *Node) {
 	for _, val := range rootp.Sons {
 		if val.Food == food {
 			node = val
@@ -34,34 +34,71 @@ func (rootp *Node) fptreeInsert(listp *Foodlist, food string) (node *Node) {
 		}
 	}
 	if node == nil {
-		node = fptreeCreadeNode(food, rootp)
+		link := list[food]
+		node = fpTreeCreadeNode(food, rootp)
 		rootp.Sons = append(rootp.Sons, node)
 		link.Links = append(list[food].Links, node)
+		list[food] = link
 	}
-	list[food] = link
 	return
 }
 
-func (rootp *Node) fptreeAppend(listp *Foodlist, foods ...string) {
+func (rootp *Node) fpTreeAppend(list Foodlist, foods ...string) {
 	cur := rootp
 	for _, food := range foods {
-		cur = cur.fptreeInsert(listp, food)
+		cur = cur.fpTreeInsert(list, food)
 	}
+}
+
+func removeItem(ds Dataset, item string) {
+	for num, orders := range ds {
+		idx := -1
+		for i, v := range orders {
+			if v == item {
+				idx = i
+				break
+			}
+		}
+		if idx > -1 {
+			ds[num] = append(orders[:idx], orders[idx+1:]...)
+		}
+	}
+}
+
+func handleSupport(ds Dataset, minSpt float64) Foodlist {
+	headTable := make(Foodlist)
+	total := float64(len(ds))
+	for _, orderList := range ds {
+		for _, food := range orderList {
+			link := headTable[food]
+			link.Count++
+			headTable[food] = link
+		}
+	}
+	for food, link := range headTable {
+		if spt := float64(link.Count) / total; spt < minSpt {
+			removeItem(ds, food)
+		}
+	}
+	return headTable
 }
 
 func main() {
 	root := Node{Food: "", Number: 0, Sons: []*Node{}}
-	nodeLink := make(Foodlist)
-	dataset := make(map[int][]string)
+	dataset := make(Dataset)
 	file, err := ioutil.ReadFile("dataset.json")
 	if err != nil {
 		log.Fatalln("Unable to read dataset.json", err)
 	}
 	json.Unmarshal(file, &dataset)
 
+	log.Println("Handle Support...")
+	minSupport := 0.2
+	headTable := handleSupport(dataset, minSupport)
+
 	log.Println("Construct FP-Tree...")
 	for _, val := range dataset {
-		root.fptreeAppend(&nodeLink, val...)
+		root.fpTreeAppend(headTable, val...)
 	}
 
 	log.Println("FP-Tree Mining...")
